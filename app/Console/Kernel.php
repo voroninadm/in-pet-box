d8c4d72b322aea9bed4bddee6452eaf7588f8b58
rinceoir <voronin.adm@mail.ru>
2023-10-03 10:26:57 +0300

    backup and its notifications added

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,8 +2,12 @@
 
 namespace App\Console;
 
+use App\Notifications\BackupCleaned;
+use App\Notifications\BackupSuccessful;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Notification;
 
 class Kernel extends ConsoleKernel
 {
@@ -12,7 +16,17 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-         $schedule->command('app:check-overdue-boxes')->daily()->at('12:00');
+        $schedule->command('app:check-overdue-boxes')->daily()->at('12:00');
+
+        $schedule->command('backup:clean')->onSuccess(function () {
+            Notification::route('telegram', 'TELEGRAM_CHAT_ID')
+                ->notify(new BackupCleaned());
+        });
+
+        $schedule->command('backup:run')->onSuccess(function () {
+            Notification::route('telegram', 'TELEGRAM_CHAT_ID')
+                ->notify(new BackupSuccessful());
+        });;
     }
 
     /**
@@ -20,7 +34,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
